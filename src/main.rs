231c8a3b33932d69e85d8f10ae85287d9ab44c9c
Plycedes <abhaypratapyadav340@gmail.com>
2024-01-18 20:23:53 +0530

    icons

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,18 @@ fn main() {
 }
 
 slint::slint! {
-    export component MainWindow inherits Window {
-        Text {
-            text: "hello world";
-            color: red;
+    component MemoryTile inherits Rectangle {
+        width: 200px;
+        height: 200px;
+        background: @linear-gradient(90deg, #fc0303, #820db8);
+
+        Image {
+            source: @image-url("./icons/bus.png");
+            width: parent.width;
+            height: parent.height;
         }
+    }
+    export component MainWindow inherits Window {
+        MemoryTile {}
     }
 }
